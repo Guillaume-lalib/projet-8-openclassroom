e26741e599128c62bb43002dea1e4578f14e8722
Laliberté Guillaume <guillaume.lalib@gmail.Com>
2023-03-09 16:11:14 +0100

    mise a jour des fichiers

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navigation from "../components/Navigation";
 import Imageabout from "../components/Imageabout";
 import Footer from "../components/Footer";
+import Collapse from "../components/Collapse";
 
 const About = () => {
   return (
     <div>
       <Navigation />
       <Imageabout />
+      <Collapse />
       <Footer />
     </div>
   );
