cd604ec8a8944f61b1dbb24c92dbf1e571ad717b
Laliberté Guillaume <guillaume.lalib@gmail.Com>
2023-03-09 17:09:05 +0100

    ajour rotation + ouverture collapse

--- my-app/src/components/Cardcollapse.js
+++ my-app/src/components/Cardcollapse.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Arrow from "../image/small-arrow.png";
 
 const Cardcollapse = (props) => {
+  const [style, setStyle] = useState();
+  const [rotateStyle, setStyleRotate] = useState();
+  const [active, setActive] = useState(false);
+  const [rotate, setRotate] = useState(false);
+  const openCollapse = () => {
+    if (!active && !rotate) {
+      setStyle({ height: "100px", opacity: 1, padding: "25px 15px" });
+      setActive(true);
+      setStyleRotate({ transform: "rotate(0deg)" });
+      setRotate(true);
+    } else {
+      setStyle();
+      setActive(false);
+      setStyleRotate();
+      setRotate(false);
+    }
+  };
+
+  //   const arrowRotate = () => {
+  //     if (!rotate) {
+  //       setActive(true);
+  //     } else {
+  //       setActive(false);
+  //     }
+  //   };
   return (
     <div className="collapse">
       <div className="cardTitle">
         <p>{props.datacard.title}</p>
-        <img src={Arrow} alt="arrow collapse" />
+        <img
+          style={rotateStyle}
+          src={Arrow}
+          alt="arrow collapse"
+          onClick={() => openCollapse()}
+        />
       </div>
-      <div className="cardText">{props.datacard.content}</div>
+      <div style={style} className="cardText">
+        {props.datacard.content}
+      </div>
     </div>
   );
 };
